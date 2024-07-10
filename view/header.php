a61afa636398cf524c01d2c22f904c411af0527e
Evgeniia  Chidester <evgeniiachidester@Evgeniias-MBP.localdomain>
2024-07-09 21:39:20 -0400

    Add folder view

---
+++ view/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>Road Tire Company</title>
+    <link rel="stylesheet" type="text/css"
+    href="/final_project/tire_company/main.css">
+
+ 
+</head>
+
+<!-- the body section -->
+<body>
+<header>
+    <h1>Welcome to the Rubber Meets the Road Tire Company</h1>
+</header>
