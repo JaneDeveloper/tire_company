dcfd055679f032aa68196c182ea70d9e24f009a3
Evgeniia  Chidester <evgeniiachidester@Evgeniias-MBP.localdomain>
2024-07-09 21:37:18 -0400

    Add folder errors and .gitignore

---
+++ errors/error.php
@@ -0,0 +1,8 @@
+
+<?php include '../view/header.php'; ?>
+<main>
+    <h1>Error</h1>
+    <p><?php echo $error; ?></p>
+    <a href="view_tire.php">View Tire</a>
+</main>
+<?php include '../view/footer.php'; ?>
